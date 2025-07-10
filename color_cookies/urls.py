74f6bda6e150eaf7d856d244318ce14c607d052d
niellecuevas <raniella810@gmail.com>
2025-07-10 23:25:49 +0800

    test

--- color_cookies/urls.py
+++ color_cookies/urls.py
@@ -7,6 +7,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cookie_app.urls')),
-    path('', TemplateView.as_view(template_name='home.html')), 
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', TemplateView.as_view(template_name='home.html')),  # Single root path
+    path('cookie/', include('cookie_app.urls')),  # Prefix other app URLs
+] + static(...)
